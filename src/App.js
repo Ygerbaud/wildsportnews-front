2315641e58a1a73f7ab855ef370df12f57bf04b6
Yohan Gerbaud <86349498+Ygerbaud@users.noreply.github.com>
2022-02-02 15:12:31 +0100

    Création Navbar avec catégories en dur

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavigationBar from './components/NavigationBar';
+import Home from './pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-    "hello world"
-    </div>
+    <BrowserRouter>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:catégories" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
