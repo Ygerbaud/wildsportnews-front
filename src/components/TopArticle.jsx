f78cdbe3988e29617b9d1b370a70f62456c13e15
Yohan Gerbaud <86349498+Ygerbaud@users.noreply.github.com>
2022-02-02 16:58:15 +0100

    Création section Top article +fecth BDD

---
+++ src/components/TopArticle.jsx
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+const TopArticle = () => {
+  const [topArticle, setTopArticle] = useState(null);
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/articles/1`)
+      .then((res) => setTopArticle(res.data[0]))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return topArticle ? (
+    <Card className="m-5" style={{ width: '70%' }}>
+      <Card.Img variant="top" src={topArticle.image} />
+      <Card.Body>
+        <Card.Title>{topArticle.titre}</Card.Title>
+        <Card.Text>{topArticle.resume}</Card.Text>
+        {/* <Button className="center" variant="outline-secondary">
+          Connexion
+        </Button> */}
+        <LinkContainer
+          className=""
+          key={topArticle.id}
+          to={`/article/${topArticle.id}`}
+        >
+          <Button className="col-md-4 text-center" variant="outline-secondary">
+            Voir la suite
+          </Button>
+        </LinkContainer>
+      </Card.Body>
+    </Card>
+  ) : null;
+};
+
+export default TopArticle;
